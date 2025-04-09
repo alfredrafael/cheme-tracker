f9369f0273188f73dd83f4db7f5ceda8f1198c40
Alfredo R. Pabon <alfredrafael@gmail.com>
2025-04-09 01:21:04 -0500

    Display page names in homepage

---
+++ sitesData/sitesData.jsx
@@ -0,0 +1,188 @@
+export const sitesData = [
+    {
+        pageName: 'ACCESS',
+        pageImage: '',
+        pageUrl: 'https://access.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Ethics for Engineers',
+        pageImage: '',
+        pageUrl: 'https://e4e.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Center for Continuous mRNA ManufacturingCenter for Continuous mRNA Manufacturing',
+        pageImage: '',
+        pageUrl: 'https://rnacenter.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'DiSTAP',
+        pageImage: '',
+        pageUrl: 'https://distap.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'MLPDS',
+        pageImage: '',
+        pageUrl: 'https://mlpds.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'D3BATT',
+        pageImage: '',
+        pageUrl: 'https://d3batt.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'CENT',
+        pageImage: '',
+        pageUrl: 'https://cent.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'AltHOST Industry Consortium',
+        pageImage: '',
+        pageUrl: 'https://althost.mit.edu',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'HIP SAT',
+        pageImage: '',
+        pageUrl: 'https://hip-sat.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Cohen Symposium',
+        pageImage: '',
+        pageUrl: 'https://cohen-symposium.mit.edu',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Robert E. Cohen Research Group',
+        pageImage: '',
+        pageUrl: 'https://cohengroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Zachary Smith Research Lab',
+        pageImage: '',
+        pageUrl: 'https://smithlab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Prahter Lab',
+        pageImage: '',
+        pageUrl: 'https://prathergroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Hatton Research Group',
+        pageImage: '',
+        pageUrl: 'https://hattongroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Myerson Research Group',
+        pageImage: '',
+        pageUrl: 'https://myersongroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Jensen Research Group',
+        pageImage: '',
+        pageUrl: 'https://jensenlab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Langer Lab',
+        pageImage: '',
+        pageUrl: 'https://langerlab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Skies Lab',
+        pageImage: '',
+        pageUrl: 'https://hsikeslab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Olsen Research Group',
+        pageImage: '',
+        pageUrl: 'https://olsenlab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Molecular Enginering Lab',
+        pageImage: '',
+        pageUrl: 'https://troutgroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'The Hammond Lab',
+        pageImage: '',
+        pageUrl: 'https://hammondlab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Strano Research Group',
+        pageImage: '',
+        pageUrl: 'https://srg.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'The Furst Lab',
+        pageImage: '',
+        pageUrl: 'https://furstlab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'The Galloway Lab',
+        pageImage: '',
+        pageUrl: 'https://gallowaylab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Coley Research Group (not ours)',
+        pageImage: '',
+        pageUrl: 'https://coley.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Rutledge Research Group',
+        pageImage: '',
+        pageUrl: 'https://rutledgegroup.mit.edu/',
+        pageWebmaster: 'Chinmay Gangal'
+    },
+    {
+        pageName: 'Qi Research Group',
+        pageImage: '',
+        pageUrl: 'https://qigroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Doyle Research Group',
+        pageImage: '',
+        pageUrl: 'https://doylegroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Metabolic Engineering Laboratory',
+        pageImage: '',
+        pageUrl: 'https://stephanopouloslab.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: 'Arup K. Chakraborty Group',
+        pageImage: '',
+        pageUrl: 'https://chakrabortygroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    },
+    {
+        pageName: '',
+        pageImage: '',
+        pageUrl: 'https://brushettresearchgroup.mit.edu/',
+        pageWebmaster: 'Unknownm'
+    }
+];
